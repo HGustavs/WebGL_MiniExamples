f03b5309012e6862cab8b1428a48e0681742d894
HGustavs <henrik.gustavsson@his.se>
2016-06-04 15:35:48 +0200

    Added crypt object and collada import cleaning code.

---
+++ emptyobj.js
@@ -0,0 +1,20 @@
+var tricount = 0;
+
+var vertices = [
+];
+
+var cubeNormal = [
+];
+
+var cubeVertexIndices = [
+];
+
+var cubeBinormal = [
+];
+
+var cubeTangent = [
+];
+
+var cubeTexcoords = [
+];
+
